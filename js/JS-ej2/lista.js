c3a501178aead10648db92e4cbf87d1cfcd3935b
Juanma Romero <xjuanma.romerox@gmail.com>
2022-06-14 09:50:21 -0400

    juntar ejercicios de OB en un repo

---
+++ js/JS-ej2/lista.js
@@ -0,0 +1,10 @@
+let nombre = "Juan Manuel Romero";
+let edad = 46;
+let desarrollador = true;
+let fechaNacimiento = new Date("november 10 1975");
+let libroFav = {
+    titulo: "El tunel",
+    autor:"Ernesto Sabato",
+    fecha: new Date("1948"),
+    url:"https://www.amazon.com/-/es/Ernesto-Sabato-ebook/dp/B006GFT5MM/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=22O6U9ERWWX1D&keywords=el+tunel&qid=1653943795&sprefix=el+tunel%2Caps%2C278&sr=8-1",
+}
---
+++ js/JS-ej3/factorial-break.js
@@ -0,0 +1,11 @@
+/* Crea los siguientes archivos JS:
+
+// - factorial-for.js ->
+    Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+
+// - factorial-while.js -> 
+    Este archivo debe calcular el factorial de 10 utilizando un bucle while
+
+// - factorial-break.js -> 
+    Este archivo debe calcular el factorial de 10 utilizando un bucle while,
+     una bifurcación if y una sentencia break */
